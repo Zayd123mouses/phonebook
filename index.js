50361066cdbfd344b56f0c43a77eda416dfed506
Zayd123mouses <theking.zaidyou@gmail.com>
2022-09-10 10:04:47 +0200

    backend

--- index.js
+++ index.js
@@ -34,11 +34,6 @@ let persons =[
     }
 ]
 
-app.get('/', (request, response) => {  
- 
-    response.send('<h1>Hello World!</h1>')
-    
-  })
   
 app.get('/api/persons', (request, response) => {   
     console.log(request);
